6c9d22c84049059c4d700047dac0cc12cc5ee18c
xarrete <giovanni.acesso@gmail.com>
2025-12-23 11:14:42 -0300

    Finaliza automatização: teste_v0

---
+++ CFEM ANM_files/apoio.js
@@ -0,0 +1,123 @@
+﻿function somenteNumero(string) {
+	var numsStr = string.replace(/[^0-9]/g, '');
+	return numsStr;
+}
+
+
+function validaCpfCnpj(valor) {
+	valor = somenteNumero(valor);
+	if (valor.length <= 11)
+		return validaCpf(valor);
+	else
+		return validaCnpj(valor);
+
+}
+
+
+/****************************************************************
+* VALIDACAO DE CPF                                              *
+*****************************************************************
+* checaCPF(document.cadastro.cpf.value)
+*/
+
+function validaCpf(CPF) {
+	if (CPF.length != 11 ||
+		CPF == "00000000000" || CPF == "11111111111" || CPF == "33333333333" ||
+			CPF == "44444444444" || CPF == "55555555555" || CPF == "66666666666" || CPF == "77777777777" ||
+				CPF == "88888888888" || CPF == "99999999999" || CPF == "01234567890") {
+		return true;
+	} else {
+		soma = 0;
+		for (i = 0; i < 9; i++) {
+			soma += parseInt(CPF.charAt(i)) * (10 - i);
+		}
+		resto = 11 - (soma % 11);
+		if (resto > 9) {
+			resto = 0;
+		}
+		if (resto != parseInt(CPF.charAt(9))) {
+			return true;
+		} else {
+			soma = 0;
+			for (i = 0; i < 10; i++) {
+				soma += parseInt(CPF.charAt(i)) * (11 - i);
+			}
+			resto = 11 - (soma % 11);
+			if (resto > 9) {
+				resto = 0;
+			}
+			if (resto != parseInt(CPF.charAt(10))) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+/****************************************************************
+* VALIDACAO DE CNPJ                                             *
+*****************************************************************
+* checaCNPJ(document.cadastro.cpf.value)
+*/
+
+function validaCnpj(CNPJ) {
+	if (CNPJ.length != 14) {
+		return true;
+	} else {
+		var dig1 = 0;
+		var dig2 = 0;
+		var x;
+		var Mult1 = '543298765432';
+		var Mult2 = '6543298765432';
+		for (x = 0; x <= 11; x++) {
+			dig1 = dig1 + (parseInt(CNPJ.slice(x, x + 1)) * parseInt(Mult1.slice(x, x + 1)));
+		}
+		for (x = 0; x <= 12; x++) {
+			dig2 = dig2 + (parseInt(CNPJ.slice(x, x + 1)) * parseInt(Mult2.slice(x, x + 1)));
+		}
+		dig1 = (dig1 * 10) % 11;
+		dig2 = (dig2 * 10) % 11;
+		if (dig1 == 10) {
+			dig1 = 0;
+		}
+		if (dig2 == 10) {
+			dig2 = 0;
+		}
+		if (dig1 != parseInt(CNPJ.slice(12, 13))) {
+			return true;
+		} else {
+			if (dig2 != parseInt(CNPJ.slice(13, 14))) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+
+function formataCpfCnpj(v) {
+	v = somenteNumero(v)
+	if (v.length <= 11)
+		return formataCpf(v);
+	else
+		return formataCnpj(v);
+}
+
+function formataCpf(v) {
+	v = v.replace(/\D/g, ""); //Remove tudo o que não é dígito
+	v = v.replace(/(\d{3})(\d)/, "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+	v = v.replace(/(\d{3})(\d)/, "$1.$2"); //Coloca um ponto entre o terceiro e o quarto dígitos
+	//de novo (para o segundo bloco de números)
+	v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); //Coloca um hífen entre o terceiro e o quarto dígitos
+	return v;
+}
+
+function formataCnpj(v) {
+	v = v.replace(/\D/g, ""); //Remove tudo o que não é dígito
+	v = v.replace(/^(\d{2})(\d)/, "$1.$2"); //Coloca ponto entre o segundo e o terceiro dígitos
+	v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3"); //Coloca ponto entre o quinto e o sexto dígitos
+	v = v.replace(/\.(\d{3})(\d)/, ".$1/$2"); //Coloca uma barra entre o oitavo e o nono dígitos
+	v = v.replace(/(\d{4})(\d)/, "$1-$2"); //Coloca um hífen depois do bloco de quatro dígitos
+	return v;
+}
---
+++ CFEM ANM_files/help.js
@@ -0,0 +1,79 @@
+﻿var htmlHelpButton = '<a style="cursor: pointer; float: right; position: relative; text-align: center;" onclick="exibeHelp()"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA2ElEQVQ4y2NgQAPiUfuNxCL3zwTi20D8G4pB7JkgOQZcQDL6ABdQ0Wwg/gvE/3FgkNxskFpsmg/j0YiOD6MYArWZWM0wPBvZz9icPRdoi4xkzAERILsGm3fAYQINMHTJ+zJxB1mAdB4QV0FdeRyLupkM0BBGlzgtHrk/DEj/BBkGNWA7FnW3GaDRhMufn4HOtABiNSD7Oxb534QMqAOGAx8OV8INuI3HgIlA3I5H/jauQIThnVCMS34mvmj8D5SzA2FcqRKetPEkpI9QjDshUSUpU5yZKMnOAN3ywB6yKg58AAAAAElFTkSuQmCC" /><br>Ajuda</a>';
+var help = {};
+var baseUrl = '/siscob/';
+
+
+if (window.location.href.indexOf('homologacaosistemas.dnpm.gov.br') != -1 || window.location.href.indexOf('homologacaoapp.dnpm.gov.br') != -1) {
+    baseUrl = 'https://homologacaoapp.dnpm.gov.br' + '/siscob/';
+    if (typeof jQuery == 'undefined') {
+        // jQuery hasn't been loaded... so let's write it into the head immediately.
+        document.write('<script type="text/javascript" src="' + 'https://homologacaoapp.dnpm.gov.br' + '/siscob/scripts/lib/jquery-1.4.1.min.js"><\/script>');
+    }
+    document.write('<script type="text/javascript" src="' + 'https://homologacaoapp.dnpm.gov.br' + '/siscob/scripts/lib/sweetalert.min.js"><\/script>');
+    document.write('<link href="' + 'https://homologacaoapp.dnpm.gov.br' + '/siscob/Content/lib/sweetalert.css" rel="stylesheet"  />')
+}
+
+else if (window.location.href.indexOf('sistemas.dnpm.gov.br') != -1 || window.location.href.indexOf('app.dnpm.gov.br') != -1) {
+    baseUrl = 'https://app.dnpm.gov.br' + '/siscob/';
+    if (typeof jQuery == 'undefined') {
+        // jQuery hasn't been loaded... so let's write it into the head immediately.
+        document.write('<script type="text/javascript" src="' + 'https://app.dnpm.gov.br' + '/siscob/scripts/lib/jquery-1.4.1.min.js"><\/script>');
+    }
+    document.write('<script type="text/javascript" src="' + 'https://app.dnpm.gov.br' + '/siscob/scripts/lib/sweetalert.min.js"><\/script>');
+    document.write('<link href="' + 'https://app.dnpm.gov.br' + '/siscob/Content/lib/sweetalert.css" rel="stylesheet"  />')
+}
+
+else {
+    if (typeof jQuery == 'undefined') {
+        // jQuery hasn't been loaded... so let's write it into the head immediately.
+        document.write('<script type="text/javascript" src="/siscob/scripts/lib/jquery-1.4.1.min.js"><\/script>');
+    }
+    document.write('<script type="text/javascript" src="/siscob/scripts/lib/sweetalert.min.js"><\/script>');
+    document.write('<link href="/siscob/Content/lib/sweetalert.css" rel="stylesheet"  />')
+}
+
+function exibeHelp() {
+    swal({
+        title: "Ajuda da página",
+        text: help.DSAjuda,
+        type: "info",
+        showCancelButton: false,
+        showconfirmButtonColor: "#DD6B55",
+        confirmButtonText: "Fechar",
+        closeOnConfirm: true,
+        html: true
+    });
+    $(".showSweetAlert").scrollTop(0);
+}
+
+function carregaHelp() {
+    var urlAtual = window.location.href;
+    var url = baseUrl + "Help/ObterPorUrl";
+    var data = { url: urlAtual };
+    $.ajax({
+        'type': "POST"
+          , 'url': url
+          , 'contentType': "application/json"
+          , 'dataType': "json"
+          , 'data': typeof data == "string" ? data : JSON.stringify(data || {})
+          //, beforeSend: function (jqXHR) {
+          //    jqXHR.setRequestHeader("X-MicrosoftAjax", "Delta=true");
+          //}
+          , 'complete': function (jqXHR, textStatus) {
+              if (textStatus == "success") {
+                  help = ((JSON && JSON.parseAjax) || $.parseJSON)(jqXHR.responseText);
+
+                  if (help != null) {
+                      $(".cabecalho-form").before(htmlHelpButton);
+                  }
+              }
+          }
+    });
+}
+setTimeout(function () {
+    $(document).ready(function () {
+        carregaHelp();
+
+    });
+
+}, 2000)
